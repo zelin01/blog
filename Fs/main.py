2799827a74ce82fa5eec9f03d56c27ebd532c54f
zelin01 <18391491732@163.com>
2026-07-02 20:45:36 +0800

    加入CRUD 依赖注入（depends)

--- Fs/main.py
+++ Fs/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
+from typing import Generator
 import sqlite3
 import os
 
@@ -27,25 +28,50 @@ def init_db():
 
 init_db()
 
+def get_db_conn() -> Generator[sqlite3.Connection, None, None]:
+    conn = get_db()
+    try:
+        yield conn
+    finally:
+        conn.close()
+
 class Post(BaseModel):
     title: str
     content: str
 
 @app.post("/posts")
-def create_post(post: Post):
-    conn = get_db()
+def create_post(post: Post, conn = Depends(get_db_conn)):
     cursor = conn.execute(
         "INSERT INTO posts (title, content) VALUES (?, ?)",
         (post.title, post.content)
     )
     conn.commit()
     post_id = cursor.lastrowid
-    conn.close()
     return {"id": post_id, "title": post.title, "content": post.content}
 
 @app.get("/posts")
-def get_posts():
-    conn = get_db()
+def get_post(conn = Depends(get_db_conn)):
     rows = conn.execute("SELECT * FROM posts").fetchall()
-    conn.close()
     return [dict(row) for row in rows]
+@app.get("/posts/{post_id}")
+def get_post(post_id: int,conn = Depends(get_db_conn)):
+    row = conn.execute("SELECT * FROM posts WHERE id = ?", (post_id,)).fetchone()
+    if row is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return dict(row)
+
+@app.put("/posts/{post_id}")
+def put_post(post_id: int, post: Post, conn = Depends(get_db_conn)):
+    cursor = conn.execute("UPDATE posts SET title = ?, content = ? WHERE id = ?", (post.title, post.content, post_id))
+    conn.commit()
+    if cursor.rowcount == 0:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return {"massage": "updated", "id": post_id}
+
+@app.delete("/posts/{post_id}")
+def delete_post(post_id: int, conn = Depends(get_db_conn)):
+    cursor = conn.execute("DELETE FROM posts WHERE id = ?", (post_id,))
+    conn.commit()
+    if cursor.rowcount == 0:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return {"message": "deleted", "id": post_id}
