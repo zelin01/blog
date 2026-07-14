0a70b2c900db2984b0cc4ee98d458628d080bf3f
zelin01 <18391491732@163.com>
2026-07-14 18:32:31 +0800

    更新数据库，由sqlite 迁移至Mysql

--- Fs/main.py
+++ Fs/main.py
@@ -80,49 +80,21 @@ def init_db():
         cursor.execute("""
                        CREATE TABLE IF NOT EXISTS users
                        (
-                           id
-                           INT
-                           AUTO_INCREMENT
-                           PRIMARY
-                           KEY,
-                           username
-                           VARCHAR
-                       (
-                           255
-                       ) UNIQUE NOT NULL,
-                           hashed_password VARCHAR
-                       (
-                           255
-                       ) NOT NULL,
-                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                           ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+                           id INT AUTO_INCREMENT PRIMARY KEY,
+                           username VARCHAR (255) UNIQUE NOT NULL,
+                           hashed_password VARCHAR(255) NOT NULL,
+                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
                        """)
         cursor.execute("""
                        CREATE TABLE IF NOT EXISTS posts
                        (
-                           id
-                           INT
-                           AUTO_INCREMENT
-                           PRIMARY
-                           KEY,
-                           title
-                           VARCHAR
-                       (
-                           255
-                       ) NOT NULL,
+                           id INT AUTO_INCREMENT PRIMARY KEY,
+                           title VARCHAR (255) NOT NULL,
                            content TEXT NOT NULL,
                            user_id INT NOT NULL,
                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                           FOREIGN KEY
-                       (
-                           user_id
-                       ) REFERENCES users
-                       (
-                           id
-                       )
-                                                                          ON DELETE CASCADE
-                           ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+                           FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCAD) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
                        """)
         conn.commit()
     print("数据库初始化完成")
