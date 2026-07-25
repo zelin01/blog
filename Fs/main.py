d610aef6bb2d99cb245576e89614a5bcd1cd24e6
zelin01 <18391491732@163.com>
2026-07-25 17:38:36 +0800

    加入了图片以及文件上传功能

--- Fs/main.py
+++ Fs/main.py
@@ -1,18 +1,26 @@
 import os
+import uuid
+import shutil
+
+from pathlib import Path
 import bcrypt
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, File, UploadFile, Form
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.responses import FileResponse
+from multipart import file_path
 from pydantic import BaseModel
 from jose import jwt, JWTError
-from typing import Optional
+from typing import Optional, Annotated
 from datetime import datetime, timedelta
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 import mysql.connector
-from mysql.connector import pooling
+from mysql.connector import pooling, cursor
 from contextlib import contextmanager
 import redis
 import json
+
+from pydantic_core.core_schema import field_after_validator_function
 
 #创建FastAPI应用
 app = FastAPI()
@@ -33,8 +41,15 @@ redis_client = redis.Redis(
     decode_responses = True
 )
 
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+
 #引入静态文件
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+
+ALLOWED_IMAGE_TYPES = {"image/jpeg", "image/png", "image/gif", "image/webp"}
+MAX_FILR_SIZE = 5 * 1024 * 1024
 
 #数据库配置
 DB_HOST = os.getenv("DB_HOST", "localhost")
@@ -116,6 +131,22 @@ def init_db():
                            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                            FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
                        """)
+        cursor.execute(""" 
+                        CREATE TABLE IF NOT EXISTS attachments (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        post_id INT NOT NULL,
+                        user_id INT NOT NULL,
+                        original_name VARCHAR (255) NOT NULL,
+                        stored_name VARCHAR (255) NOT NULL UNIQUE,
+                        file_path VARCHAR (500) NOT NULL,
+                        file_size INT NOT NULL,
+                        content_type VARCHAR (100) NOT NULL,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+                        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+                        INDEX idx_post_id (post_id))
+                        ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+                        """)
         conn.commit()
     print("数据库初始化完成")
 
@@ -175,6 +206,24 @@ def get_current_user(token: str = Depends(oauth2_scheme), db=Depends(get_db_conn
     if row is None:
         raise credentials_exception
     return row
+
+def validate_image(file: UploadFile) -> None:
+    if file.content_type not in ALLOWED_IMAGE_TYPES:
+        raise HTTPException(status_code=400, detail="Invalid image type")
+    content = file.file.read()
+    if len(content) > MAX_FILR_SIZE:
+        raise HTTPException(status_code=400, detail="File too big")
+    file.file.seek(0)
+    return content
+
+def generate_unique_filename(original_name: str) -> None:
+    ext = Path(original_name).suffix
+    return f"{uuid.uuid4().hex}{ext}"
+
+def clear_post_cache(post_id: int):
+    redis_client.delete("posts/{post_id}")
+    redis_client.delete("posts:list")
+    redis_client.delete(f"attachments:post:{post_id}")
 
 #pydantic 数据模型
 #用于请求参数校验和响应列化
@@ -237,7 +286,6 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db=Depends(get_db_co
 @app.post("/posts")
 def create_post(post: Post, db=Depends(get_db_conn), current_user=Depends(get_current_user)):
     conn, cursor = db
-
     #插入文章， user_id 为当前登录用户
     cursor.execute(
         "INSERT INTO posts (title, content, user_id) VALUES (%s, %s, %s)",
@@ -245,7 +293,6 @@ def create_post(post: Post, db=Depends(get_db_conn), current_user=Depends(get_cu
     )
     conn.commit()
     post_id = cursor.lastrowid
-
     # 清除文章列表缓存（因为新增了文章）
     redis_client.delete("posts:list")
     return {"message": "created", "id": post_id}
@@ -337,8 +384,136 @@ def delete_post(post_id: int, db=Depends(get_db_conn), current_user=Depends(get_
     conn.commit()
     return {"message": "deleted", "id": post_id}
 
+@app.post("/post/{post_id/upload")
+async def upload_image(
+        post_id: int,
+        file: UploadFile = File(..., description="Upload image"),
+        db = Depends(get_db_conn),
+        current_user=Depends(get_current_user)
+        ):
+    conn, cursor = db
 
-if __name__ == "__main__":
-    import uvicorn
+    cursor.execute(f"SELECT * FROM posts WHERE id = %s", (post_id,))
+    post = cursor.fetchone()
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    if post["user_id"] != current_user["id"]:
+        raise HTTPException(status_code=403, detail="Not authorized to upload this post")
+    validate_image(file)
+    stored_name = generate_unique_filename(file.filename)
+    file_path = UPLOAD_DIR / stored_name
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    cursor.execute("""INSERT INTO attachments (post_id, user_id, original_name,stored_name, file_path, file_size, content_type)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)""",(
+                    post_id,
+                    current_user["id"],
+                    file.filename,
+                    stored_name,
+                    str(file_path),
+                    file_path.stat().st_size,
+                    file.content_type
+                    ))
+    conn.commit()
+    attchment_id = cursor.lastrowid
+
+    clear_post_cache(post_id)
+    return{
+        "message": "Image uploaded successfully",
+        "attchment_id": attchment_id,
+        "filename": file.filename,
+        "url": f"/uploads/{stored_name}",  # 通过静态文件服务访问
+        "size": file_path.stat().st_size
+    }
+
+@app.get("/post/{post_id}/attachments")
+def get_post_attchments(post_id: int, db=Depends(get_db_conn)):
+    cache_key = f"attachments:post:{post_id}"
+    cached = redis_client.get(cache_key)
+    if cached:
+        return json.loads(cached)
+
+    conn, cursor = db
+    cursor.execute("""
+                   SELECT id, original_name, stored_name, file_size, content_type, created_at
+                   FROM attachments
+                   WHERE post_id = %s
+                   ORDER BY created_at ASC
+                   """, (post_id,))
+    rows = cursor.fetchall()
+
+    # 拼接完整访问 URL
+    for row in rows:
+        row["url"] = f"/uploads/{row['stored_name']}"
+
+    redis_client.setex(cache_key, 300, json.dumps(rows, default=str))
+    return rows
+
+@app.delete("/attachments/{attachment_id}")
+def delete_attachment(
+    attachment_id: int,
+    db=Depends(get_db_conn),
+    current_user=Depends(get_current_user)
+):
+    """
+    删除指定图片
+    - 图片上传者或文章作者均可删除
+    """
+    conn, cursor = db
+
+    # 查询图片信息
+    cursor.execute("""
+        SELECT a.*, p.user_id as post_owner_id
+        FROM attachments a
+        JOIN posts p ON a.post_id = p.id
+        WHERE a.id = %s
+    """, (attachment_id,))
+    att = cursor.fetchone()
+
+    if att is None:
+        raise HTTPException(status_code=404, detail="Attachment not found")
+    if att["user_id"] != current_user["id"] and att["post_owner_id"] != current_user["id"]:
+        raise HTTPException(status_code=403, detail="Not authorized to delete this attachment")
+
+    file_path = Path(att["file_path"])
+    if file_path.exists():
+        file_path.unlink()
+
+    # 删除数据库记录
+    cursor.execute("DELETE FROM attachments WHERE id = %s", (attachment_id,))
+    conn.commit()
+
+    # 清理缓存
+    clear_post_cache(att["post_id"])
+
+    return {"message": "Attachment deleted", "id": attachment_id}
+
+@app.get("/attachments/{attachment_id}")
+def get_attachment_file(attachment_id: int, db=Depends(get_db_conn)):
+    """通过 attachment_id 直接访问原图"""
+    conn, cursor = db
+    cursor.execute("SELECT file_path, original_name, content_type FROM attachments WHERE id = %s", (attachment_id,))
+    row = cursor.fetchone()
+    if row is None:
+        raise HTTPException(status_code=404, detail="Attachment not found")
+
+    return FileResponse(
+        path=row["file_path"],
+        filename=row["original_name"],
+        media_type=row["content_type"]
+    )
+
+@app.post("/files/")
+async def creat_file(file: Annotated[bytes | None, File()] = None):
+    if not file:
+        return {"message": "No file set"}
+    return {"file_size": len(file)}
+
+
+@app.post("/uploaadfile/")
+async def creat_upload_file(file: UploadFile | None = None):
+    if not file:
+        return {"message": "No file set"}
+    return {"filename": file.filename}
